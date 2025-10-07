83c8b52c661493798f282f73b0d0fa99d475d81e
Aswin Biju <aswinit0722gmail.com>
2025-10-08 00:06:06 +0530

    Edited Home Header

--- src/Components/HomeHeader/index.js
+++ src/Components/HomeHeader/index.js
@@ -3,7 +3,7 @@ import Avatar from "../../assets/Images/Avatar.png";
 
 const HomeHeader = () => {
     return (
-        <div className="flex justify-between p-4 bg-black md:p-">
+        <div className="flex justify-between p-4 bg-black md:px-12">
             <div className="flex justify-between items-center">
                 <span className="m-1 text-white font-bold md:text-2xl md:m-2">☰</span>
                 <p className="m-1 text-white font-bold md:text-2xl md:m-2">SERVICE CONNECT</p>
